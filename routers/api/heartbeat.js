034f0b8da3f1b2f06a12503c758314d53b598a27
杨刚 <yanggang@feinno.com>
2018-12-12 18:04:59 +0800

    * init

--- routers/api/heartbeat.js
+++ routers/api/heartbeat.js
@@ -1,8 +1,4 @@
-/**
- * 心跳
- *
- * Created by VLER on 2018/7/18.
- */
+const fs = require("fs");
 const moment = require('moment');
 const tools  = require('../../utils/tools');
 
@@ -17,8 +13,26 @@ module.exports = function (router) {
     router.post('/tt', async(ctx)=>{
        console.log(ctx.request.body);
 
-       let r = await tools.git('diff v001.txt');
+       let r = await tools.git('diff ./data/v001.txt ./data/v002.txt');
        console.log('git' ,r);
        ctx.body = r;
+    });
+
+    router.get('/t1', async(ctx)=>{
+        let v1 = fs.readFileSync('./data/v001.txt');
+        console.log('t1' ,v1);
+        ctx.body = v1.toString();
+    });
+
+    router.get('/t2', async(ctx)=>{
+        let v1 = fs.readFileSync('./data/v001.txt');
+        console.log('t1' ,v1.toString());
+
+        let lines = v1.toString().split('\n');
+
+        console.log('lines', lines);
+
+
+       await ctx.render('index',{lines:lines, content:v1.toString()});
     });
 };
